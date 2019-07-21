7661b81c7ead7734ada2626ad434c64950a3eaa9
unknown <uchpolinet@gmail.com>
2019-07-21 18:07:08 +1000

    add a user input form for a search bar with a submit button, and created an array and buttons of topics of jokes

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  // initial array of three different types of jokes
+  var jokes = ["Programmer Jokes", "Asian Parent Jokes", "Asian Kids Jokes"];
+  console.log(jokes);
+});
