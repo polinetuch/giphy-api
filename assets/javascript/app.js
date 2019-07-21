591cae7200c768906b9e753294b76fb1ca7a6bfa
unknown <uchpolinet@gmail.com>
2019-07-21 21:36:41 +1000

    created an array of topics and used jquery to display them as buttons in browser

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,31 @@
 $(document).ready(function() {
-  $("button").on("click", function() {
+  // array of topics to display as buttons
+  var topics = ["dracarys", "send help", "like a boss"];
+
+  // function for displaying array as buttons
+  function displayArrButtons() {
+    $("#arrayBtn").empty();
+
+    // looping through the length of the array
+    for (var i = 0; i < topics.length; i++) {
+      //display each string as a button in the browser
+      $("#arrayBtn").append(
+        '<button class="btn-primary">' + topics[i] + "</button>"
+      );
+    }
+  }
+
+  // call the function to see the buttons in browser
+  displayArrButtons();
+
+  $("document").on("click", "#displayGifHere", function() {
+    event.preventDefault();
+
     // grabbing and storing the data-joke property value from the button
-    var jokes = $(this).attr("data-joke");
-    console.log(jokes);
+    var jokes = $("#displayGifHere")
+      .val()
+      .trim();
+    topics.push(jokes);
 
     // create a queryURL using the topic of jokes
     var queryURL =
@@ -24,12 +47,25 @@ $(document).ready(function() {
       var results = response.data;
 
       // loop through each result item
-      // for (var i = 0; i < results.length; i++) {
+      for (var i = 0; i < results.length; i++) {
+        // create a new div to store the response
+        var jokeDiv = $("<div>");
 
-      //     // create a new div to store the response
-      //     var jokeDiv = $("<div>");
+        // creat a paragraph tag to hold the result of the rating for each gif
+        var p = $("<p>").text("Rating: " + results[i].rating);
 
-      // }
+        // store the image in an image tag
+        var jokeImage = $("<img>");
+
+        jokeImage.attr("src", results[i].images.fixed_height.url);
+
+        // append the rating to the img tag along with the gifs
+        jokeDiv.append(p);
+        jokeDiv.append(jokeImage);
+
+        // prepend the jokeDiv to the HTML page in the "#displayGifHere" div
+        $("#displayGifHere").append(jokeDiv);
+      }
     });
   });
 });
