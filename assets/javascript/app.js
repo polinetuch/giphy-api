2d5dc1745640b7fbbb6581dd2bd581ddaac01a1b
unknown <uchpolinet@gmail.com>
2019-07-21 18:33:19 +1000

    created an on click function for attributes of data-joke and get request and response from ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
-  // initial array of three different types of jokes
-  var jokes = ["Programmer Jokes", "Asian Parent Jokes", "Asian Kids Jokes"];
-  console.log(jokes);
+  $("button").on("click", function() {
+    // grabbing and storing the data-joke property value from the button
+    var jokes = $(this).attr("data-joke");
+    console.log(jokes);
+
+    // create a queryURL using the topic of jokes
+    var queryURL =
+      "http://api.giphy.com/v1/gifs/search?q=" +
+      jokes +
+      "&api_key=ZGa0mMJYIhqY7pqFoGuZwABAXMLx88zU&limit=10";
+
+    // perform an ajax request with the queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+
+      // after data comes back from the request
+    }).then(function(response) {
+      console.log(queryURL);
+      console.log(response);
+
+      // store the data from AJAX in this variable named results
+      var results = response.data;
+
+      // loop through each result item
+      // for (var i = 0; i < results.length; i++) {
+
+      //     // create a new div to store the response
+      //     var jokeDiv = $("<div>");
+
+      // }
+    });
+  });
 });
