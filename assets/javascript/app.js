20ed8505e877fdf24e9a51074ce129e45180183a
unknown <uchpolinet@gmail.com>
2019-07-24 23:40:04 +1000

    added more styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,71 +1,40 @@
 $(document).ready(function() {
-  // array of topics to display as buttons
-  var topics = ["dracarys", "send help", "like a boss"];
-
-  // function for displaying array as buttons
-  function displayArrButtons() {
-    $("#arrayBtn").empty();
-
-    // looping through the length of the array
-    for (var i = 0; i < topics.length; i++) {
-      //display each string as a button in the browser
-      $("#arrayBtn").append(
-        '<button class="btn-primary">' + topics[i] + "</button>"
-      );
-    }
+  var gif = ["books", "flower", "stars"];
+  for (var i = 0; i < gif.length; i++) {
+    $("#arrayBtn").append("<button class='gif-button'>" + gif[i] + "</button>");
   }
 
-  // call the function to see the buttons in browser
-  displayArrButtons();
+  // $("#gif-button").on("click", function() {
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    gif +
+    "&api_key=ZGa0mMJYIhqY7pqFoGuZwABAXMLx88zU&limit=10";
 
-  $("document").on("click", "#displayGifHere", function() {
-    event.preventDefault();
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(queryURL);
+    console.log(response);
 
-    // grabbing and storing the data-joke property value from the button
-    var jokes = $("#displayGifHere")
-      .val()
-      .trim();
-    topics.push(jokes);
+    var result = response.data;
 
-    // create a queryURL using the topic of jokes
-    var queryURL =
-      "http://api.giphy.com/v1/gifs/search?q=" +
-      jokes +
-      "&api_key=ZGa0mMJYIhqY7pqFoGuZwABAXMLx88zU&limit=10";
+    for (var i = 0; i < result.length; i++) {
+      $(".gif-button").attr("src", result[i].images.fixed_height.url);
+      console.log(result);
 
-    // perform an ajax request with the queryURL
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-
-      // after data comes back from the request
-    }).then(function(response) {
-      console.log(queryURL);
-      console.log(response);
-
-      // store the data from AJAX in this variable named results
-      var results = response.data;
-
-      // loop through each result item
-      for (var i = 0; i < results.length; i++) {
-        // create a new div to store the response
-        var jokeDiv = $("<div>");
-
-        // creat a paragraph tag to hold the result of the rating for each gif
-        var p = $("<p>").text("Rating: " + results[i].rating);
-
-        // store the image in an image tag
-        var jokeImage = $("<img>");
-
-        jokeImage.attr("src", results[i].images.fixed_height.url);
-
-        // append the rating to the img tag along with the gifs
-        jokeDiv.append(p);
-        jokeDiv.append(jokeImage);
-
-        // prepend the jokeDiv to the HTML page in the "#displayGifHere" div
-        $("#displayGifHere").append(jokeDiv);
-      }
-    });
+      var pTag = $("<p>").text("Rating: " + result[i].rating);
+      var newImg = $("#displayGifHere").attr(
+        "src",
+        result[i].images.fixed_height.url
+      );
+      newImg.append(pTag);
+    }
   });
+
+  // $("#submitBtn").on("click", function() {
+  //   var inputBtn = "";
+  //   inputBtn =
+  // })
+  // });
 });
